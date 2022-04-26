5d77e4aea601017b453c983e7d18709d6eeb0ee6
Castle <39484343+aremu-dan@users.noreply.github.com>
2022-04-27 00:08:26 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,77 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import SearchDiv from './SearchDiv'
+import AppData from './content/Appdata.json'
 
 function App() {
+
+  // http://api.weatherapi.com/v1
+  // d9e43eb252a60dc4bda2e5401c07f1e0
+
+  // console.log(AppData);
+
+  const [Weather, setWeather] = useState({temp:''})
+  const [Data, setData] = useState({
+    location: '',
+    isSearched: false
+  })
+
+  
+  useEffect(()=>{
+
+    // // Current Weather API Endpoint
+    // http://api.weatherstack.com/current?access_key=YOUR_ACCESS_KEY&query=LAGOS
+    // // optional parameters: 
+    // & units = m
+    // & language = en
+    // & callback = MY_CALLBACK
+
+    // console.log(url);
+  }, [Weather]);
+
+
+  const handleInput = (e) => {
+    setData(prevState => {
+      return {...prevState, [e.target.name]: e.target.value}
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(Data.location !== ''){
+      getData();
+    }
+
+    // setData(prevState => {
+    //   return {...prevState, isSearched:true}
+    // }, [getData()])
+  }
+
+  async function getData() {
+    const url = AppData.url + 
+      AppData.secret_key + 
+      AppData.query + 
+      Data.location;
+      
+    // My Implementation
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+
+    if(!responseJSON.success){
+      setData(prevState => {
+        return {...prevState, isSearched:true}
+      })
+    }else{
+      setData(prevState => {
+        return {...prevState, isSearched:false}
+      })
+    }
+
+    console.log(responseJSON);
+  }
+
+
   return (
     <div className='App'>
       <div className='device'>
@@ -9,9 +80,13 @@ function App() {
           <p>Enter the name of a city :</p>
         </div>
         <div className='body'>
-          <input type='text' className='inputField'/>
-          <br/>
-          <button type="submit">submit</button>
+          <form  onSubmit={handleSubmit}>
+            <input type='text' className='inputField' name='location' placeholder='Eg. London, Lagos, New York' value={Data.location} onChange={handleInput}/>
+            <button type="submit">submit</button>
+          </form>
+
+          {/*  */}
+          {Data.isSearched && <SearchDiv Weather={Weather} />}
         </div>
       </div>
     </div>
---
+++ src/SearchDiv.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SearchDiv({isSearched, Weather}) {
+   return (
+      <div className='weatherDisplay'>
+         <span>
+            The weather in lagos is currently 'overcast clouds'. The temperature is 28°C and the wind speed is 2.78m/s
+         </span>
+      </div>
+   )
+}
+
+export default SearchDiv
